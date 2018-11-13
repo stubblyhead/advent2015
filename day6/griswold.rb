7d8f3ef034d43afbff1fec33a877343f85c8f9d2
Mike Stevenson <Mike.Stevenson@us.logicalis.com>
2018-11-13 15:40:38 -0800

    part 1 complete

--- day6/griswold.rb
+++ day6/griswold.rb
@@ -1,3 +1,7 @@
+require 'pry'
+
+binding.pry
+
 class Light
   attr_reader :bulb
 
@@ -16,12 +20,51 @@ class Light
   def toggle
     @bulb = !@bulb
   end
+
+  def ==(other)
+    @bulb == other
+  end
 end
 
-mylight = Light.new
-mylight.turn_on
-puts mylight.bulb
-mylight.turn_off
-puts mylight.bulb
-mylight.toggle
-puts mylight.bulb
+instructions = File.readlines('./input', :chomp=> true)
+display = Array.new(1000) { Array.new(1000) { Light.new } }
+
+def turn_on(x1, y1, x2, y2, grid)
+  grid[y1..y2].each do |i|
+    i[x1..x2].each { |j| j.turn_on }
+  end
+end
+
+def turn_off(x1, y1, x2, y2, grid)
+  grid[y1..y2].each do |i|
+    i[x1..x2].each { |j| j.turn_off }
+  end
+end
+
+def toggle(x1, y1, x2, y2, grid)
+  grid[y1..y2].each do |i|
+    i[x1..x2].each { |j| j.toggle }
+  end
+end
+
+instructions.each do |i|
+  match = i.match(/([\w\s]+) (\d+),(\d+) through (\d+),(\d+)/)
+  x1 = match[2].to_i
+  y1 = match[3].to_i
+  x2 = match[4].to_i
+  y2 = match[5].to_i
+
+  case match[1]
+  when 'toggle'
+    toggle(x1, y1, x2, y2, display)
+  when 'turn on'
+    turn_on(x1, y1, x2, y2, display)
+  when 'turn off'
+    turn_off(x1, y1, x2, y2, display)
+  end
+end
+
+lightcount = 0
+display.each { |i| lightcount += i.count(true) }
+
+puts lightcount
