c1d551aa7dca6dd12bc32dd0ccfc6b4b3bbc723c
Mike Stevenson <Mike.Stevenson@us.logicalis.com>
2018-11-13 16:28:10 -0800

    class coercion!

--- day6/griswold.rb
+++ day6/griswold.rb
@@ -1,11 +1,11 @@
 require 'pry'
 
-binding.pry
+#binding.pry
 
 class Light
   attr_reader :bulb
-  def initialize
-    @bulb = 0
+  def initialize(val=0)
+    @bulb = val
   end
 
   def turn_on
@@ -13,7 +13,7 @@ class Light
   end
 
   def turn_off
-    [0,@bulb-1].max
+    @bulb = [0,@bulb-1].max
   end
 
   def toggle
@@ -26,6 +26,24 @@ class Light
 
   def to_i
     @bulb.to_i
+  end
+
+  def to_s
+    @bulb.to_s
+  end
+
+  def +(other)
+    if other.is_a? Light
+      Light.new(@bulb + other.bulb)
+    elsif other.is_a? Integer
+      Light.new(@bulb + other)
+    else
+      raise TypeError, "#{other.class} can't be coerced into Light"
+    end
+  end
+
+  def coerce(other)
+    [Light.new(other), self]
   end
 end
 
@@ -68,6 +86,6 @@ instructions.each do |i|
 end
 
 lightcount = 0
-display.each { |i| lightcount += i.count(true) }
+display.each { |i| lightcount += i.sum }
 
 puts lightcount
