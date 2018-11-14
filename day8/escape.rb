08e690d4f0ca47841cb8aa52522ccad727f95de1
Mike Stevenson <Mike.Stevenson@us.logicalis.com>
2018-11-14 10:46:40 -0800

    maybe it's this simple, maybe it ain't

---
+++ day8/escape.rb
@@ -0,0 +1,10 @@
+words = File.readlines('./testcase', :chomp => true)
+codelength = 0
+charlength = 0
+
+words.each do |i|
+  codelength += i.length
+  charlength += eval("#{i}").length
+end
+
+puts codelength - charlength
