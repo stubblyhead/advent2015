24337002e5cf9dc26bf6f9eb1813e26ad2254472
Mike Stevenson <Mike.Stevenson@us.logicalis.com>
2018-11-16 12:00:46 -0800

    was using the wrong bit of the match data in a few places and wasn't slicing the string right to check for the second double letter

--- day11/password.rb
+++ day11/password.rb
@@ -2,9 +2,9 @@ def valid?(str)
   return false if str.match(/i|l|o/) #any pw with these letters is invalid
   match = str.match(/(\w)\1/)
   if match
-    double = str.index(match[1]) #find where double letter starts
-    temp = str[0..double] + str[double+2..-1] #get candidate without the double letter
-    return false unless !temp.match(match[1]) and temp.match(/(/w)\1/) #bail early unless
+    double = str.index(match[0]) #find where double letter starts
+    temp = str[0,double] + str[double+2..-1] #get candidate without the double letter
+    return false unless !temp.match(match[0]) and temp.match(/(\w)\1/) #bail early unless
     #candidate doesn't contain the first double letter and does contain a different double letter
   else
     return false  #no double letters at all
