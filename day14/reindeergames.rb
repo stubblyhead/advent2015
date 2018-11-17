2e33b32b9dc815dbd47857fee98e0620bb70848b
Mike Stevenson <Mike.Stevenson@us.logicalis.com>
2018-11-16 23:23:21 -0800

    part 2 complete

--- day14/reindeergames.rb
+++ day14/reindeergames.rb
@@ -1,31 +1,51 @@
+require 'pry'
+binding.pry
+
 class Reindeer
-  attr_reader :speed, :run_time, :rest_time, :distance
+  attr_reader :speed, :run_time, :rest_time, :distance, :elapsed_time, :points
 
   def initialize(speed, run_time, rest_time)
     @speed = speed
     @run_time = run_time
     @rest_time = rest_time
+    @elapsed_time = 0
+    @distance = 0
+    @points = 0
   end
 
-  def run(time)
-    cycles = time / (@run_time + @rest_time)
-    leftover = time % (@run_time + @rest_time)
-    @distance = cycles * @speed * @run_time
-    @distance += [@run_time, leftover].min * speed
+  def running?
+    @elapsed_time % (@run_time + @rest_time) < @run_time
+  end
+
+  def add_point
+    @points += 1
+  end
+
+  def run
+    @distance += @speed if running?
+    @elapsed_time += 1
   end
 
 end
-field = {}
+field = []
 lines = File.readlines('./input', :chomp => true)
 lines.each do |i|
-  match = i.match(/^(\w+).* (\d+).* (\d+).* (\d+)/)
-  name, speed, run, rest = match[1,4]
-  field[name] = Reindeer.new(speed.to_i, run.to_i, rest.to_i)
+  match = i.match(/.* (\d+).* (\d+).* (\d+)/)
+  speed, run, rest = match[1,3]
+  field.push (Reindeer.new(speed.to_i, run.to_i, rest.to_i))
 end
 
-field.each_value { |i| i.run(2503) }
+2503.times { |t|
+  distances = field.map do |i|
+    i.run
+    i.distance
+  end
+  max_distance = distances.max
+  distances.each_with_index do |x,i|
+    if x == max_distance
+      field[i].add_point
+    end
+  end
+}
 
-results = field.values
-results.map! { |i| i = i.distance }
-
-puts results.max
+puts field.map { |i| i.points }.max
