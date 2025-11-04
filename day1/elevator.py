4c20f33a127a1ce7bc22acbc37fc72d264d97038
stubblyhead <27823133+stubblyhead@users.noreply.github.com>
2025-11-03 16:00:15 -0800

    might be a better way to do this?

--- day1/elevator.py
+++ day1/elevator.py
@@ -3,3 +3,15 @@ if __name__ == '__main__':
     with open('input') as f:
         inst = f.read()
         print(inst.count('(') - inst.count(')'))
+
+    floor = 0
+    for i in range(len(inst)):
+        if inst[i] == '(':
+            floor += 1
+        elif inst[i] == ')':
+            floor -= 1
+            if floor < 0:
+                print(i+1)
+                break   
+        else:
+            print(f"unexpected char, found {inst[i]}")
--- elevator.py
+++
@@ -1,5 +0,0 @@
-if __name__ == '__main__':
-
-    with open('input') as f:
-        inst = f.read()
-        print(inst.count('(') - inst.count(')'))
