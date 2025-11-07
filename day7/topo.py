1820277a82522b495a66ee6ea7956848c7639c93
stubblyhead <27823133+stubblyhead@users.noreply.github.com>
2025-11-06 22:25:39 -0800

    kicking 2018 self

--- day7/topo.py
+++ day7/topo.py
@@ -1,8 +1,41 @@
+def topo_sort(graph):
+    result_list = []
+    no_incoming = []
+    remaining_edges = []
+    for k,v in graph.items():
+        if v[0] == []:
+            no_incoming.append(k)
+        else:
+            remaining_edges += v[0]
+    while no_incoming:
+        current_v = no_incoming.pop(0)
+        outgoing_edges = []
+        result_list.append(current_v)
+        edges_to_remove = []
+        for e in remaining_edges:
+            if e[0] == current_v:
+                outgoing_edges.append(e)
+                edges_to_remove.append(e)
+        for e in edges_to_remove:
+            remaining_edges.remove(e)
+        for e in outgoing_edges:
+            in_count = get_incoming_edge_count(remaining_edges, e[1])
+            if in_count == 0:
+                no_incoming.append(e[1])
+    return result_list
+
+def get_incoming_edge_count(edgelist, vertex):
+    edge_count = 0
+    for e in edgelist:
+        if e[1] == vertex:
+            edge_count += 1
+    return edge_count
+
 if __name__ == '__main__':
     with open('input') as f:
         instructions = f.readlines()
     graph = {}
-    adj_list = []
+    wires = {}
     for i in instructions:
         l,r = i.strip().split(' -> ')
         if l.isnumeric():
@@ -22,36 +55,32 @@ if __name__ == '__main__':
         else:
             graph[r] = ([[l,r]],l)
 
-    def topo_sort(graph):
-        def get_incoming_edge_count(edgelist, vertex):
-            edge_count = 0
-            for e in edgelist:
-                if e[1] == vertex:
-                    edge_count += 1
-            return edge_count
-        result_list = []
-        no_incoming = []
-        remaining_edges = []
-        for k,v in graph.items():
-            if v[0] == []:
-                no_incoming.append(k)
+    order = topo_sort(graph)
+
+    for o in order:
+        inst = graph[o][-1]
+        if type(inst) == int:
+            wires[o] = int(inst)
+        elif inst.count('AND'):
+            inst = inst.split()
+            if inst[0].isnumeric():
+                wires[o] = int(inst[0]) & wires[inst[-1]]
             else:
-                remaining_edges += v[0]
-        while no_incoming:
-            current_v = no_incoming.pop(0)
-            outgoing_edges = []
-            result_list.append(current_v)
-            edges_to_remove = []
-            for e in remaining_edges:
-                if e[0] == current_v:
-                    outgoing_edges.append(e)
-                    edges_to_remove.append(e)
-            for e in edges_to_remove:
-                remaining_edges.remove(e)
-            for e in outgoing_edges:
-                in_count = get_incoming_edge_count(remaining_edges, e[1])
-                if in_count == 0:
-                    no_incoming.append(e[1])
-        return result_list
-    
-    
+                wires[o] = wires[inst[0]] & wires[inst[-1]]
+        elif inst.count('OR'):
+            inst = inst.split()
+            wires[o] = wires[inst[0]] | wires[inst[-1]]
+        elif inst.count('LSHIFT'):
+            inst = inst.split()
+            wires[o] = wires[inst[0]] << int(inst[-1])
+        elif inst.count('RSHIFT'):
+            inst = inst.split()
+            wires[o] = wires[inst[0]] >> int(inst[-1])
+        elif inst.count('NOT'):
+            inst = inst.split()
+            wires[o] = 65535 - int(wires[inst[-1]])
+        else:
+            inst = inst.split()
+            wires[o] = wires[inst[0]]
+
+    print(wires['a'])
