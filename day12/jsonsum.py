218c8ebd24fac0ab8316ab51d82f376b09089707
stubblyhead <27823133+stubblyhead@users.noreply.github.com>
2025-11-07 22:55:26 -0800

    wow object_hook was basically tailor made for this

--- day12/jsonsum.py
+++ day12/jsonsum.py
@@ -1,10 +1,22 @@
 import json
 import re
 
+def obj_hook(s):
+    if 'red' in s.values():
+        return {}
+    else:
+        return s
+
 if __name__ == '__main__':
     with open('input') as f:
         data = f.readline().strip()
 
     nums = re.findall(r'-?\d+',data)
     nums = list(map(int,nums))
-    print(sum(nums))    
+    print(sum(nums))
+
+    json_data = json.loads(data,object_hook=obj_hook)
+    no_red = json.dumps(json_data)
+    nums = re.findall(r'-?\d+', no_red)
+    nums = list(map(int,nums))
+    print(sum(nums))
