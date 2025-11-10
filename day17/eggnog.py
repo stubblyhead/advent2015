99c166803472c74af758544a45ff32c5e1070842
stubblyhead <27823133+stubblyhead@users.noreply.github.com>
2025-11-09 17:45:35 -0800

    i guess maybe it wasn't then

--- day17/eggnog.py
+++ day17/eggnog.py
@@ -4,12 +4,16 @@ if __name__ == '__main__':
     with open('input') as f:
         cups = list(map(int, f.readlines()))
 
-    valid = 0
+    valid = { i: 0 for i in range(4,12) }
     # need at least 4 cups and no more than 11 to get to 150
     for i in range(4,12):
         combos = combinations(cups, i)
-        for i in combos:
-            if sum(i) == 150:
-                valid += 1
+        for j in combos:
+            if sum(j) == 150:
+                valid[i] += 1
 
-    print(valid)
+    print(sum(valid.values()))
+    for k,v in valid.items():
+        if v > 0:
+            print(v)
+            break
