f8cf89395e18a60f48e55392cddeea657702443e
stubblyhead <27823133+stubblyhead@users.noreply.github.com>
2025-11-09 21:32:03 -0800

    I swear to fucking god list mutability will be the fucking death of me

--- day18/mikeylikesit.py
+++ day18/mikeylikesit.py
@@ -16,7 +16,9 @@ class Light:
 
 class Grid:
     def __init__(self, grid):
-        self.grid = grid
+        self.grid = []
+        for g in grid:
+            self.grid.append(list(g))
 
     def count_neighbors(self, row, col):
         count = 0
@@ -41,36 +43,61 @@ class Grid:
             out_str += '\n'
         return out_str
 
-            
+class BrokenGrid(Grid):
+    def __init__(self,grid):
+        super().__init__(grid)
+        for r in [0,len(self.grid)-1]:
+            for c in [0,len(self.grid[r])-1]:
+                grid[r][c].lighted = True
+    
+    def toggle_all(self):
+        for r in [0,len(self.grid)-1]:
+            for c in [0,len(self.grid[r])-1]:
+                self.grid[r][c].toggle = False
+        super().toggle_all()
+
 
 if __name__ == '__main__':
     with open('input') as f:
         lines = f.readlines()
 
     grid = []
+    brokengrid = []
+
     tr_table = str.maketrans('.#','01')
     for l in lines:
         l = l.translate(tr_table).strip()
 
         grid.append(list(map(Light,l)))
+        brokengrid.append(list(map(Light,l)))
 
     grid = Grid(grid)
-    print(grid)
-    for _ in range(100
-                   ):
+    brokengrid = BrokenGrid(brokengrid)
+    for _ in range(100):
         for r in range(len(grid.grid)):
             for c in range(len(grid.grid[r])):
                 neighbors = grid.count_neighbors(r,c)
+                b_neighbors = brokengrid.count_neighbors(r,c)
                 if grid.grid[r][c].lighted and neighbors not in [2,3]:
                     grid.grid[r][c].toggle = True
                 elif not grid.grid[r][c].lighted and neighbors == 3:
                     grid.grid[r][c].toggle = True
+                if brokengrid.grid[r][c].lighted and b_neighbors not in [2,3]:
+                    brokengrid.grid[r][c].toggle = True
+                elif not brokengrid.grid[r][c].lighted and b_neighbors == 3:
+                    brokengrid.grid[r][c].toggle = True
         grid.toggle_all()
+        brokengrid.toggle_all()
 
 
     on_lights = 0
     for r in grid.grid:
         for l in r:
             on_lights += l.lighted
+    b_lights = 0
+    for r in brokengrid.grid:
+        for l in r:
+            b_lights += l.lighted
 
     print(on_lights)
+    print(b_lights)
