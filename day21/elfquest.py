6c3d934d8a102f5887a48215054e5ce2b29834e6
stubblyhead <27823133+stubblyhead@users.noreply.github.com>
2025-11-15 22:01:32 -0800

    You hit the elf.  The elf dies.

--- day21/elfquest.py
+++ day21/elfquest.py
@@ -1,3 +1,5 @@
+from itertools import combinations
+
 class Character:
     def __init__(self, hp, dmg, ac):
         self.hp = hp
@@ -38,12 +40,57 @@ def fight(player, boss):
 
 if __name__ == '__main__':
     with open('input') as f:
+        lines = f.readlines() 
+    boss_hp = int(lines[0].split(': ')[1])
+    boss_dmg = int(lines[1].split(': ')[1])
+    boss_ac = int(lines[2].split(': ')[1])
+    weapons = {}
+    armor = {}
+    rings = {}
+    
+    with open('weapons') as f:
         lines = f.readlines()
-        boss_hp = int(lines[0].split(': ')[1])
-        boss_dmg = int(lines[1].split(': ')[1])
-        boss_ac = int(lines[1].split(': ')[1])
-        # boss = Character(boss_hp, boss_dmg, boss_ac)
-        boss = Character(12,7,2)      
-        player = Character(8,5,5)
+    for l in lines:
+        name, cost, dmg, ac = l.split()
+        weapons[name] = Item(int(cost),int(dmg),int(ac))
 
-        print(fight(player, boss))
+    with open('armor') as f:
+        lines = f.readlines()
+    for l in lines:
+        name, cost, dmg, ac = l.split()
+        armor[name] = Item(int(cost),int(dmg),int(ac))
+
+    with open('rings') as f:
+        lines = f.readlines()
+    for l in lines:
+        name, cost, dmg, ac = l.split()
+        rings[name] = Item(int(cost),int(dmg),int(ac))
+
+
+    
+    min_cost = 356
+
+    for w in weapons.values():
+        for a in armor.values():
+            for r in rings.values():
+                boss = Character(boss_hp, boss_dmg, boss_ac)
+                this_cost = w.cost + a.cost + r.cost
+                player = Character(100,0,0)
+                for x in [w,a,r]:
+                    player.get_item(x)
+                if fight(player, boss) == 'player':
+                    min_cost = min([min_cost, this_cost])
+            for c in combinations(rings.values(),2):
+                boss = Character(boss_hp, boss_dmg, boss_ac)
+                this_cost = w.cost + a.cost
+                player = Character(100,0,0)
+                for r in c:
+                    this_cost += r.cost
+                for x in [w,a]+list(c):
+                    player.get_item(x)
+                if fight(player,boss) == 'player':
+                    min_cost = min([min_cost, this_cost])
+    print(min_cost)
+                
+
+    
